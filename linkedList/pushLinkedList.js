07fbd57d07fcdc88d5982c3de38ec79f21ac4e01
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2026-05-11 18:18:06 -0300

    feat: implement linked list with push and pop methods

---
+++ linkedList/pushLinkedList.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let myLinkedList = new LinkedList(4);
+console.log(myLinkedList);
+myLinkedList.push(11);
+myLinkedList.push(3);
+console.log(myLinkedList);
