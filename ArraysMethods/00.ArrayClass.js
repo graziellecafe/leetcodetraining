7b16b6b4c6a69cb8dcc83e4c1ddd2fbfb4c8565b
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2026-05-28 12:44:01 -0300

    feat: implement MyArray class with push and pop methods

---
+++ ArraysMethods/00.ArrayClass.js
@@ -0,0 +1,33 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("hello");
+
+console.log(newArray);
+
+console.log(newArray.pop());
+console.log(newArray);
