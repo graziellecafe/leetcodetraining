113b1b7497fea7f9cab3ff4790f0a23ee4d383e9
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2026-06-30 12:41:58 -0300

    feat: add functions to check for common items between two arrays

---
+++ 07.HashTables/01.InterviewQuestion01.js
@@ -0,0 +1,15 @@
+function itemInCommon(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+let arr1 = [1, 3, 5];
+let arr2 = [2, 4, 5];
+
+const hasItemInCommon = console.log(itemInCommon(arr1, arr2));
